dd31f5daf099a5945dcf526eb10d1fb8becb0916
mrange3 <matthew.range91@gmail.com>
2018-11-16 20:27:48 -0500

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friendsArray= [
+    {
+        "name": "Eric Forman",
+        "photo": "https://goo.gl/images/IW0z7v",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Steven Hyde",
+        "photo": "https://goo.gl/images/FRgTAs",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Fez",
+        "photo": "https://goo.gl/images/b7EgPk",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Jackie Burkhart",
+        "photo": "https://goo.gl/images/6iEKBB",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Michael Kelso",
+        "photo": "https://goo.gl/images/92w6zw",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Donna Pinciotti",
+        "photo": "https://goo.gl/images/xBiK6w",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+
+];
+
+module.exports = friendsArray;
